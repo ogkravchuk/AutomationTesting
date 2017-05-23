b3f8cd1ebc0b2e7751a2a0d23fb4023aca0841d9
ogkravchuk <ogkravchuk@ukr.net>
2017-05-24 00:07:35 +0300

    Check plz

--- Lesson3/Advansed/Task1_1.java
+++ Lesson3/Advansed/Task1_1.java
@@ -6,23 +6,22 @@ public class Task1_1 {
 
 	
     public static String genPass(int leng) {
-        String pass  = ""; // initialize var
-        Random arr = new Random(); // instance of random array
-        int string = leng + arr.nextInt(leng); // in var string, which were entered previous + 1 of switch 
-
-        for (int i = 0; i < string; i++) { // run iteraction
-            char value = 0; // char value is
+        String pass  = ""; 
+        Random arr = new Random(); 
+        int string = leng + arr.nextInt(leng); 
+        for (int i = 0; i < string; i++) { 
+            char value = 0; 
             int quontOfVal = 15; 
             
-            switch(arr.nextInt(3)) { // switch
-                case 0: {value = '0'; quontOfVal = 10;} // pay attention in names
+            switch(arr.nextInt(3)) { 
+                case 0: {value = '0'; quontOfVal = 10;} 
                      break;
                 case 1: {value = 'a'; quontOfVal = 26;} 
                      break;
                 case 2: {value = 'A'; quontOfVal = 26;} 
                      break;
             }
-            pass += (char)((arr.nextInt(quontOfVal)) + value); // describe what type and what quantity will in pass var, without space
+            pass += (char)((arr.nextInt(quontOfVal)) + value); 
         }
         return pass;
     }
