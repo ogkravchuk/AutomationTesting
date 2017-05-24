1785740c8a8314af0bf30d0700898abe3561e034
ogkravchuk <ogkravchuk@ukr.net>
2017-05-24 23:08:55 +0300

    test

---
+++ Lesson3/Advansed/Test.java
@@ -0,0 +1,19 @@
+package Advansed;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+		Workbook wb = new HSSFWorkbook();
+		Sheet sheet=wb.createSheet("mySheet");
+		FileOutputStream fos = new FileOutputStream("my.xls");
+		wb.write(fos);
+		fos.close();
+}
+}
