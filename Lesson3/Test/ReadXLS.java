e728b4555c00b0d83d4af6bb7aa08f133a27e803
ogkravchuk <ogkravchuk@ukr.net>
2017-05-26 22:44:54 +0300

    xls

---
+++ Lesson3/Test/ReadXLS.java
@@ -0,0 +1,30 @@
+package Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ReadXLS {
+	public void readXLS() throws IOException{
+		Workbook wb = new HSSFWorkbook(new FileInputStream("D:/Учеба/read.xls"));
+		String result = wb.getSheetAt(0).getRow(0).getCell(0).getStringCellValue();
+		System.out.println(result);
+	}
+public static void main(String[] args) throws IOException{
+ReadXLS One = new ReadXLS();
+One.readXLS();
+}
+}
+
+/*public static void main(String[] args) throws IOException{
+	FileInputStream fis= new FileInputStream("D:/Учеба/read.xls");
+	Workbook wb = new HSSFWorkbook(fis);
+	String result = wb.getSheetAt(0).getRow(0).getCell(0).getStringCellValue();
+	System.out.println(result);
+	fis.close();
+}
+}
+	*/
--- Lesson3/Test/Test.java
+++ Lesson3/Test/Test.java
@@ -1,4 +1,4 @@
-package Advansed;
+package Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
